d37dd6cfff3ef69fef86a4f96990ea722fc35a56
VladimirStavinchuk <sun10@mail.ru>
2021-08-21 15:29:24 +0300

    add news with bugs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $(function(){
 
+//   $('.slider__inner, .news__slider-inner').slick({
+//     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+//     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+//     infinite: false
+//   });
+
   $('.slider__inner, .news__slider-inner').slick({
-    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-    infinite: false
-  });
+   nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+   prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+   infinite: false
+ });
 
   $('select').styler();
 
